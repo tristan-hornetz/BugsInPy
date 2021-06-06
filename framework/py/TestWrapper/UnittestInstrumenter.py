88e01049f76fe37cbc8b1519cd5332dcf326b0da
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-06-06 19:56:19 +0200

    Only modify files during unittest instrumentation if unittest was detected

--- framework/py/TestWrapper/UnittestInstrumenter.py
+++ framework/py/TestWrapper/UnittestInstrumenter.py
@@ -11,6 +11,10 @@ WRAPPER_BASE = 'TestWrapper.WrapperBase'
 
 class InputTransformer(Transformer):
 
+    def __init__(self, *args, **kwargs):
+        super(InputTransformer, self).__init__(*args, **kwargs)
+        self.modified = False
+
     def visit_Import(self, node: Import) -> Any:
         names = list(((n.name, n.asname) for n in node.names))
         for (n, asn) in names:
@@ -20,12 +24,14 @@ class InputTransformer(Transformer):
                     unittest_alias = alias(name=n.replace(UNITTEST, WRAPPER_BASE), asname=asn)
                 else:
                     unittest_alias = alias(name=WRAPPER_BASE, asname=UNITTEST)
+                self.modified = True
                 return Import(old_aliases + [unittest_alias])
         return node
 
     def visit_ImportFrom(self, node: ImportFrom) -> Any:
         if node.module.split('.')[0] == UNITTEST:
             module_name = node.module.replace(UNITTEST, WRAPPER_BASE)
+            self.modified = True
             return ImportFrom(module_name, node.names, node.level)
         return node
 
@@ -41,5 +47,6 @@ parsed = ast.parse(file_text)
 t = InputTransformer()
 t.visit(parsed)
 
-with open(file_path, 'wt') as f:
-    f.write(ast.unparse(parsed))
+if t.modified:
+    with open(file_path, 'wt') as f:
+        f.write(ast.unparse(parsed))

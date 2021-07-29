53e608bed77ef5bf71928191959713bd711bbcea
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-07-29 22:03:12 +0200

    Updated httpie alltest

--- projects/httpie/alltest.sh
+++ projects/httpie/alltest.sh
@@ -6,6 +6,7 @@ ln -s "$(readlink -f httpie)" tests/httpie
 if [[ "$buggy_commit" == "16df8848e81eefac830f407e4b985f42b52970da" ]]; then
   echo "Different test setup"
   echo "from TestWrapper.WrapperBase import pytest_runtest_call, pytest_sessionfin" >> tests/conftest.py
+  ln -s $(readlink -f TestWrapper) tests/TestWrapper
   pytest tests/*
 else
   pytest

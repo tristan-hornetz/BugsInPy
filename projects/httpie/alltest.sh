8e2c1efecb69c1f979f2a1c4d67b9f180e36061f
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-07-29 21:58:20 +0200

    Updated httpie alltest

--- projects/httpie/alltest.sh
+++ projects/httpie/alltest.sh
@@ -1,3 +1,11 @@
 
 ln -s "$(readlink -f httpie)" tests/httpie
-pytest
+
+ln -s "$(readlink -f httpie)" tests/httpie
+
+if [[ "$buggy_commit" == "16df8848e81eefac830f407e4b985f42b52970da" ]]; then
+  echo "Different test setup"
+  pytest tests/tests.py
+else
+  pytest
+fi

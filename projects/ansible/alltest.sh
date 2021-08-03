efd39e9f77a3eebd5b039cb2bc78ee3be52486d5
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-08-03 21:07:34 +0200

    Updated ansible alltest.sh

--- projects/ansible/alltest.sh
+++ projects/ansible/alltest.sh
@@ -2,4 +2,6 @@
 pip install -r test/units/requirements.txt
 pip install pytest-xdist mock pytest-mock
 source hacking/env-setup
-ansible-test units --local
+ln -s $(readlink -f TestWrapper) ./lib/TestWrapper
+ln -s $(readlink -f WrapClass.py) ./lib/WrapClass.py
+pytest -n 8 --continue-on-collection-errors

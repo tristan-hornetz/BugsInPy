5d9e301849ecc657ad1aa936415300749ae48c63
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-08-18 19:07:53 +0200

    Updated matplotlib alltest.sh

--- projects/matplotlib/alltest.sh
+++ projects/matplotlib/alltest.sh
@@ -1,5 +1,6 @@
 
-pip install pytest-xdist
+pip install pytest-xdist Cython
+python -mpip install -ve . -I
 ln -s $(readlink -f WrapClass.py) lib/WrapClass.py
 ln -s $(readlink -f TestWrapper) lib/TestWrapper
-pytest -n 10
+pytest -n 8

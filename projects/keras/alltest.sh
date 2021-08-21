f6630009f0ed0a6d614d1d51efea1cfc0bda6062
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-08-21 17:12:33 +0200

    Updated keras alltest.sh

--- projects/keras/alltest.sh
+++ projects/keras/alltest.sh
@@ -1,4 +1,17 @@
 
 sed -i 's/-n 2/-n 8/g' pytest.ini
 
+pip freeze > _temp.txt
+DONE=false
+until $DONE ;do
+read || DONE=true
+if [ "$REPLY" != "" ]; then
+   if [[ "$REPLY" == "Keras==2.2.2"* ]]; then
+       pip uninstall Keras-Applications tensorflow --yes
+       pip install Keras-Applications==1.0.7 tensorflow==1.14.0
+   fi
+fi
+done < "_temp.txt"
+rm _temp.txt
+rm -f keras/utils/conftest.py
 pytest

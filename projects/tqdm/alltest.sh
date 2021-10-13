ebf0e3d88e6145a0f340a26eac29d0b26ce47fd2
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-10-13 14:59:59 +0200

    Modiefied tqdm alltest

--- projects/tqdm/alltest.sh
+++ projects/tqdm/alltest.sh
@@ -8,7 +8,6 @@ do
   ln -s "$(readlink -f "${line_arr[1]}")" "$fdname/test_$bname"
 done < <(du -a | grep ".*/tests_.*py$")
 
-pip install pytest-xdist
 ln -s $(readlink -f ./tqdm) ./tqdm/tests/tqdm
 
-pytest -n 8
+pytest --ignore-glob=tqdm/tests/tqdm
